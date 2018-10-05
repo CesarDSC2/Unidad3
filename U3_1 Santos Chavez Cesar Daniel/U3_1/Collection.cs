c7e8831650e39282ce6805510a701bbef3ab28b9
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-10-05 12:51:15 -0700

    Add files via upload

---
+++ U3_1 Santos Chavez Cesar Daniel/U3_1/Collection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace U3_1
+{
+    class Collection : CollectionBase
+    {
+        public void Add(Object item)
+        {
+            InnerList.Add(item);
+        }
+
+        public void Remove(Object item)
+        {
+            InnerList.Remove(item);
+        }
+
+        public new void Clear()
+        {
+            InnerList.Clear();
+        }
+
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+
+    }
+}
---
+++ U3_1 Santos Chavez Cesar Daniel/U3_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+//ViewModel
+
+namespace U3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection nombres = new Collection();
+            nombres.Add("Cesar");
+            nombres.Add("Julio");
+            nombres.Add("Arturo");
+            nombres.Add(2);
+            foreach (var item in nombres)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+
+            nombres.Remove("Arturo");
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+            nombres.Clear();
+            Console.WriteLine(nombres.Count());
+            Console.ReadKey();
+
+            ArrayList lista = new ArrayList();
+            lista.Add("Pedro");
+            lista.Add("Goku");
+            lista.Add("Jiren");
+            lista.Add("Raso");
+
+            if (lista.Contains("Pedro"));
+            {
+                Console.WriteLine("Si aparece aquí");
+            }
+            Console.ReadKey();
+
+            lista.Sort();
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+
+            lista.Reverse();
+            Console.WriteLine();
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
