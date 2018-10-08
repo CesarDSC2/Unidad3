4be099ba05b6eaabb8d8ba06c477fb92d10ffbef
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-10-08 12:53:28 -0700

    Add files via upload

---
+++ Clase de pilas/ConsoleApp1/Pilas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Pilas
+    {
+        private int p_index;
+        private ArrayList list; //Los nombres de las clases deben ir en mayúscula y en singular.
+
+        public Pilas()
+        {
+            list = new ArrayList();
+            p_index = -1;
+        }
+
+        public int Count //Es una variable
+        {
+            get { return list.Count; } //Es un identificador de acceso que obtiene el dato el dato publico de una variable
+        }
+
+        public void Push(object item)
+        {
+            list.Add(item);
+            p_index++;
+        }
+
+        public object Pop() //Va a regresar lo que sea que este en la lista en el p_index
+        {
+            object obj = list[p_index];
+            list.Remove(p_index);
+            p_index--;
+            return obj;
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            p_index = -1;
+        }
+
+        public object Peek()
+        {
+            return list[p_index];
+        }
+    }
+}
---
+++ Clase de pilas/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilas lista = new Pilas();
+
+            for (int i = 0; i < 5; i++)
+            {
+                lista.Push(i + 1);
+            }
+
+            /*Console.WriteLine(lista.Count);
+            Console.ReadKey();
+            Console.WriteLine(lista.Peek());
+            Console.ReadKey();
+            Console.WriteLine(lista.Pop());
+            Console.WriteLine(lista.Peek());
+            */
+            Console.ReadKey();
+            string character;
+            string palabra = "sees";
+            bool isPalindrome = true;
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                //Extrae los elementos del string de acuerdo al indice y a la cantidad de elementos y los agrega a la pila
+                lista.Push(palabra.Substring(i, 1));
+                int posicion = 0;
+                while (lista.Count > 0)
+                {
+                    character = lista.Pop().ToString();
+                }
+            }
+        }
+    }
+}
